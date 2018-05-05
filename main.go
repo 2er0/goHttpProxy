8d7314c61b93f291c494c3e9311352f92e017e9e
David Baumgartner <2er0@dbaumi.at>
2018-05-05 08:08:16 +0200

    added logging to stdout

--- main.go
+++ main.go
@@ -4,10 +4,13 @@ import (
 	"github.com/elazarl/goproxy"
 	"log"
 	"net/http"
+	"os"
 )
 
 func main() {
 	proxy := goproxy.NewProxyHttpServer()
 	proxy.Verbose = true
+	proxy.Logger = log.New(os.Stdout, "Proxy", log.LstdFlags)
+
 	log.Fatal(http.ListenAndServe(":8080", proxy))
 }
