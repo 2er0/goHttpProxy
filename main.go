ccf69d38e6cedddc3755ece4b8640b942dcba1c1
David Baumgartner <2er0@dbaumi.at>
2018-07-05 19:25:36 +0200

    test fix

--- main.go
+++ main.go
@@ -4,12 +4,13 @@ import (
 	"github.com/elazarl/goproxy"
 	"log"
 	"net/http"
+	"os"
 )
 
 func main() {
 	httpProxy := goproxy.NewProxyHttpServer()
 	httpProxy.Verbose = true
-	//httpProxy.Logger = log.New(os.Stdout, "", log.LstdFlags)
+	httpProxy.Logger = log.New(os.Stdout, "", log.LstdFlags)
 
 	log.Println("Starting proxy")
 
